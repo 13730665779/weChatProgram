e1db91cacc6443bece0e7aa9a54533b168eb3d68
Wangtingting152 <39662863+Wangtingting152@users.noreply.github.com>
2018-08-23 09:03:40 +0800

    Update app.js

--- 后台管理模块/app.js
+++ 后台管理模块/app.js
@@ -2,28 +2,56 @@ var express = require('express');
 var app=express();
 var fs = require("fs");
 
-var multer = require('multer');
+// var multer = require('multer');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({limit:'50mb',extended:true}));
 app.use(express.static('public'));
-app.set("views engine",'ejs');
+app.set("view engine",'ejs');
 app.set('views',__dirname+ '/views');
 app.use(bodyParser.urlencoded({ extended: false }));
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// *显示*
 app.get('/index', function (req, res) {
-    res.render('index',{});
+    //风格信息
+    var ThemeUtil_WTT=require('./dao/ThemeUtil_WTT');
+    var theme=new ThemeUtil_WTT();
+    theme.init();
+    var QQutil2=require('./util/QQutil2');
+    var qqutil=new QQutil2();
+    qqutil.init();
+    //房间信息
+    var RoomUtil = require("./dao/RoomUtil");
+    var room = new RoomUtil();
+    room.init();
+    room.queryAll(function (roomData) {
+        var length=roomData.length;
+        for (var i=0;i<length;i++){
+            // console.log(roomData[i]);
+            // roomBuffer.push(roomData[i]);
+        }
+        theme.queryAll(function (themeData) {
+            var length = themeData.length;
+            for (var i = 0; i < length; i++) {
+                var themeKey = themeData[i].imgKey;
+                qqutil.query(themeKey, function (url) {
+                    themeData[i].imgKey = url;
+                })
+            }
+            res.render('index', {
+                themeData:themeData,
+                roomData:roomData
+            });
+        })
+
+    })
+
+
+
 })
-
-
-// app.post('/upload', urlencodedParser, multer({dest: __dirname + '/public/upload/'}).array('file'), function (req, res) {
-//     var themeName = req.body.themeName;
-//     var themeInfor = req.body.themeInfor;
-//     var themeImgKey = req.file.imgKey.path;
-//     console.log(themeName+" "+themeInfor+ " "+ themeImgKey);
-// })
+//*风格的添加*
 var formidable = require('formidable');
-app.post("/index",function (req, res) {
+app.post("/upload",function (req, res) {
     var form = new formidable.IncomingForm();   //创建上传表单
     form.encoding = 'utf-8';        //设置编辑
     form.uploadDir = 'public/upload';     //设置上传目录
@@ -59,7 +87,7 @@ app.post("/index",function (req, res) {
             return;
         }
 
-        console.log(fields.themeName+","+fields.themeInfor+" ," +files.uploadImage.path);
+        // console.log(fields.themeName+","+fields.themeInfor+" ," +files.uploadImage.path);
         //显示地址
         var path = files.uploadImage.path;
         // var index=  path.lastIndexOf('\\')+1;
@@ -67,21 +95,79 @@ app.post("/index",function (req, res) {
         // console.log(path);
         //图片主键
         var fileKey = "nodejs"+new Date().getTime()+"img";
-        console.log(fileKey);
+        // console.log(fileKey);
         var QQutil2=require('./util/QQutil2');
         var qqutil=new QQutil2();
         qqutil.init();
         qqutil.insert(fileKey,path,fs,function() {
             var themeName = fields.themeName;
             var themeInfor = fields.themeInfor;
-
+            var ThemeUtil_WTT=require('./dao/ThemeUtil_WTT');
+            var theme=new ThemeUtil_WTT();
+            theme.init();
+            theme.inserTheme(themeName,themeInfor,fileKey);
         });
-        
+
         res.json({
-            "newPath":'http://localhost:8882/index/'+path
+            "newPath":'http://localhost:8883/upload/'+path
         });
     });
 });
+//*删除风格*
+app.post('/deleteTheme',urlencodedParser,function (req,res) {
+    var themeId=req.body.themeId;
+    var ThemeUtil_WTT = require("./dao/ThemeUtil_WTT");
+    var Theme = new ThemeUtil_WTT();
+    Theme.init();
+    Theme.deleteTheme(themeId);
+})
+// *房间添加*
+app.post("/addRoom",urlencodedParser,function(req,res){
+    var roomNo = req.body.roomNo;
+    var themeName=req.body.themeName;
+    var accomodate = req.body.accomodate
+    var roomPrice = req.body.roomPrice;
+    var ThemeUtil_WTT=require('./dao/ThemeUtil_WTT');
+    var theme=new ThemeUtil_WTT();
+    theme.init();
+    var id;
+    theme.queryThemeID(themeName,function (themeId) {
+        id=themeId[0].id;
+        var RoomUtil = require("./dao/RoomUtil");
+        var room = new RoomUtil();
+        room.init();
+        room.inserRoom(roomNo,id,accomodate,roomPrice);
+    })
 
-console.log("http://localhost:8882/index");
-var server = app.listen(8882);
+})
+// *删除房间*
+app.post('/deleteRoom',urlencodedParser,function (req,res) {
+   var roomNo=req.body.roomNo;
+    var RoomUtil = require("./dao/RoomUtil");
+    var room = new RoomUtil();
+    room.init();
+    room.deleteRoom(roomNo);
+})
+// *修改房间*
+app.post("/updateRoom",urlencodedParser,function (req,res) {
+    var id = req.body.id;
+    var name = req.body.name;
+    var value = req.body.value;
+    var RoomUtil = require("./dao/RoomUtil");
+    var room = new RoomUtil();
+    room.init();
+    if (name=="themeId") {
+        room.updateTheme(value,id);
+    }
+    else if(name=="accommodate"){
+        room.updateAccommodate(value,id);
+    }
+    else if(name=="roomPrice"){
+        room.updateRoomPrice(value,id);
+    }
+
+
+})
+
+console.log("http://localhost:8883/index");
+var server = app.listen(8883);
